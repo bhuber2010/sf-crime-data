13f04bc2fbe2d46075d88af11344de9394aaf611
bhuber2010 <bhuber@alumni.colostate.edu>
2016-09-06 17:05:44 -0700

    feedback working, but no img due to base64

--- srv/app.js
+++ srv/app.js
@@ -1,20 +1,31 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const github = new require('github')()
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({
+  limit: '5000kb'
+}))
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
+  limit: '5000kb'
 }))
 app.use(cors())
 
 app.use(express.static(__dirname + '/../public'))
 
+github.authenticate({
+    type: 'oauth',
+    token: process.env.GHTOKEN
+})
+
 app.post('/feedback', function(req, res) {
-  console.log(req.body)
-  res.end()
+  console.log(req.body.feedback)
+  createGHissue(req.body.feedback)
+  res.sendStatus(200)
 })
 
 app.set('port', process.env.PORT || 3333);
@@ -22,3 +33,22 @@ app.set('port', process.env.PORT || 3333);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
+function createGHissue(data) {
+  var browserData = `<pre> ${JSON.stringify(data.browser, null, 4)} </pre>`
+  var message = `<p>Feedback: ${data.note}</p>`
+  var img = `<img src='${data.img}'></img>`
+  var html = `${browserData} <br> ${message} <br>`
+  console.log(html);
+  return github.issues.create({
+    user: 'sfbrigade',
+    repo: 'sf-crime-data',
+    title: 'User Feedback',
+    body: html,
+    labels: ['Type: User Feedback'],
+    assignees: []
+  }, function (err, data) {
+    console.log(data)
+    return data
+  })
+}
